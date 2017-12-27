2991fccfeb021c132c9fbac0d9c670cfa1815529
Todd Vanderlin <todd@vanderlin.cc>
2017-12-27 12:00:19 -0500

    mobile update

--- src/components/IndexPage.vue
+++ src/components/IndexPage.vue
@@ -11,7 +11,7 @@
 
 <script>
 import CardSVG from '../assets/xmass-2017-01.svg'
-
+import Hammer from 'hammerjs'
 export default {
     name: 'IndexPage',
     props: {},
@@ -36,29 +36,72 @@ export default {
             }
         },
         initCard() {
-            svgPanZoom(this.$refs.svg.$el, {
-            panEnabled: true,
-                /*
-                controlIconsEnabled: false
-                , zoomEnabled: true
-                , dblClickZoomEnabled: true
-                , mouseWheelZoomEnabled: true
-                , preventMouseEventsDefault: true
-                , zoomScaleSensitivity: 0.2
-                , minZoom: 0.5
-                , maxZoom: 10
-                , fit: true
-                , contain: false
-                , center: true
-                , refreshRate: 'auto'
-                , beforeZoom: function(){}
-                , onZoom: function(){}
-                , beforePan: function(){}
-                , onPan: function(){}
-                , onUpdatedCTM: function(){}
-                , customEventsHandler: {}
-                , eventsListenerElement: null*/
-            });
+
+            // Don't use window.onLoad like this in production, because it can only listen to one function.
+        var eventsHandler = {
+            haltEventListeners: ['touchstart', 'touchend', 'touchmove', 'touchleave', 'touchcancel'], 
+            init: function(options) {
+                var instance = options.instance;
+                var initialScale = 1
+                var pannedX = 0
+                var pannedY = 0
+                // Init Hammer
+                // Listen only for pointer and touch events
+                this.hammer = Hammer(options.svgElement, {
+                    inputClass: Hammer.SUPPORT_POINTER_EVENTS ? Hammer.PointerEventInput : Hammer.TouchInput
+                })
+                
+                // Enable pinch
+                this.hammer.get('pinch').set({enable: true})
+            
+                // Handle double tap
+                this.hammer.on('doubletap', function(ev){
+                    instance.zoomIn()
+                })
+                
+                // Handle pan
+                this.hammer.on('panstart panmove', function(ev){
+                    // On pan start reset panned variables
+                    if (ev.type === 'panstart') {
+                        pannedX = 0
+                        pannedY = 0
+                    }
+                    // Pan only the difference
+                    instance.panBy({x: ev.deltaX - pannedX, y: ev.deltaY - pannedY})
+                    pannedX = ev.deltaX
+                    pannedY = ev.deltaY
+                })
+                
+                // Handle pinch
+                this.hammer.on('pinchstart pinchmove', function(ev){
+                    // On pinch start remember initial zoom
+                    if (ev.type === 'pinchstart') {
+                        initialScale = instance.getZoom()
+                        instance.zoom(initialScale * ev.scale)
+                    }
+                    instance.zoom(initialScale * ev.scale)
+                })
+                
+                // Prevent moving the page on some devices when panning over SVG
+                options.svgElement.addEventListener('touchmove', function(e){ e.preventDefault(); });
+            }, 
+            destroy: function() {
+                this.hammer.destroy()
+            }
+        }
+
+        // Expose to window namespace for testing purposes
+        svgPanZoom(this.$refs.svg.$el, {
+            zoomEnabled: true, 
+            controlIconsEnabled: true, 
+            fit: 1, 
+            center: 1, 
+            customEventsHandler: eventsHandler
+        });
+
+            /*svgPanZoom(this.$refs.svg.$el, {
+                panEnabled: true,
+            });*/
 
             var el = $(this.$refs.svg.$el).hide();
             el.delay(1000).fadeIn(3000)
